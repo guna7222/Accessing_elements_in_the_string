9e7e6c3db36bf37dbb04fa5d07a276ec38f943f0
guna7222 <47294637+guna7222@users.noreply.github.com>
2021-01-07 22:13:33 +0530

    Initial commit

---
+++ Accessing_elements_in_string.py
@@ -0,0 +1,11 @@
+# In order to access the elements in the string brackets are used []
+""" Brackets can be used
+to accress the
+elements in the string """
+# python index position will count from zero(0)
+
+programming = 'python'
+print(programming[0])
+
+
+
